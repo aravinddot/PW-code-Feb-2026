3bab1fa6264bc5ddef8f0aadb24331268a0ad21f
Aravind112703 <aravindmathi93@gmail.com>
2026-03-05 21:03:47 +0530

    advanced 3

--- tests/sandoxAdvanced.spec.ts
+++ tests/sandoxAdvanced.spec.ts
@@ -127,9 +127,9 @@ test.describe('Interactive Playwright Sandbox Advanced', () => {
     test('Handling File Upload ', async ({ page }) => {
         await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
 
-        // await page.getByTestId('file-upload-input').setInputFiles('uploads/test.txt')
+        await page.getByTestId('file-upload-input').setInputFiles('uploads/test.txt')
 
-        // await expect(page.getByText('test.txt uploaded successfully.')).toBeVisible()
+        await expect(page.getByText('test.txt uploaded successfully.')).toBeVisible()
 
 
         await page.getByTestId('multi-file-upload-input').setInputFiles(['uploads/practice-data.csv', 'uploads/practice-report (1).pdf'])
@@ -151,11 +151,101 @@ test.describe('Interactive Playwright Sandbox Advanced', () => {
             page.getByTestId('download-pdf-btn').click()
         ])
 
-        const download = await downloadFile
-        const fileName = download.suggestedFilename()
-        await download.saveAs(`downloads/${fileName}`)
+        //const download = await downloadFile
+        const fileName = await downloadFile.suggestedFilename()
+        await downloadFile.saveAs(`downloads/${fileName}`)
 
         await expect(page.getByText('practice-report.pdf download started.')).toBeVisible()
+
+    })
+
+
+
+    //practice-iframe
+
+
+
+    test('Handling Iframe', async({page})=> {
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        const iframe = await page.frameLocator('#practice-iframe')
+
+        await iframe.locator('#frame-input').fill('Playwright')
+
+        await iframe.locator('#frame-save').click()
+
+        await expect(iframe.locator('#frame-result')).toContainText('Playwright saved')
+
+    })
+
+
+
+    test('Handling shadow DOM', async({page})=> {
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        const shadow = await page.getByTestId('shadow-host')
+
+        await shadow.locator('#shadow-input').fill('Playwright')
+
+        await shadow.locator('#shadow-save').click()
+
+        await expect(shadow.getByText('Result: Playwright saved')).toBeVisible()
+
+    })
+
+
+
+
+
+    test('Practice and Interview Dates', async({page})=> {
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        // await page.getByTestId('practice-date-picker').type('01-11-1995')
+
+        // await expect(page.getByText('Practice Date Selected: 1995-11-01')).toBeVisible()
+
+        //   await page.getByTestId('practice-date-picker').fill('1995-11-01')
+
+        // await expect(page.getByText('Practice Date Selected: 1995-11-01')).toBeVisible()
+
+        const datePicker = await page.getByTestId('interview-date-picker')
+
+        await datePicker.evaluate((dom, val)=> {
+
+            const html = dom as HTMLInputElement
+            html.value = val as string
+            html.dispatchEvent(new Event('input'))
+            html.dispatchEvent(new Event('change'))
+
+        }, "1995-11-01")
+
+        await expect(page.getByText('Interview Date Selected: 1995-11-01')).toBeVisible()
+
+    })
+
+
+
+
+
+    test('wait commands', async({page})=> {
+//https://playwright-mastery-academy-app.vercel.app/practice/popup?source=waitfornavigation
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        await page.getByTestId('wait-navigation-link').click()
+
+        await page.waitForURL('https://playwright-mastery-academy-app.vercel.app/practice/popup?source=waitfornavigation')
+
+        await expect(page.getByText('Popup Opened Successfully')).toBeVisible()
+
+        await page.getByTestId('wait-response-btn').click()
+        await page.waitForResponse('https://playwright-mastery-academy-app.vercel.app/api/practice/waits-status')
+        await expect(page.getByText('Trigger API Response Completed')).toBeVisible()
+
+
 
     })
 
